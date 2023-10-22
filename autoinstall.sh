f3a56bcbbeae477c7b8f4dc77298d4d4bbcdb4d0
julianobarbosa <julianomb@gmail.com>
2023-10-22 12:26:08 -0300

    feat(nala): added kubectl and k9s

--- autoinstall.sh
+++ autoinstall.sh
@@ -101,7 +101,7 @@ sleep 5
 
 # Install Nala Package Manager, Z Shell, Termux Clipboard, Neovim, NodeJS, Python-pip, Ruby, wget, logo-ls, Timewarrior, Taskwarrior, htop
 pkg update && pkg install nala -y
-nala install termux-api neovim nodejs python-pip ruby luarocks luajit ripgrep fd lazygit wget gettext logo-ls ncurses-utils libuv timewarrior taskwarrior htop -y || error_exit "${RED}Failed to install packages.${ENDCOLOR}"
+nala install termux-api neovim nodejs python-pip okc-agents ruby luarocks luajit ripgrep fd lazygit wget kubectl k9s gettext logo-ls ncurses-utils libuv timewarrior taskwarrior htop -y || error_exit "${RED}Failed to install packages.${ENDCOLOR}"
 
 # Install pynvim, pnpm and neovim npm package, and neovim gem package
 pip install pynvim || error_exit "${RED}Failed to install pynvim.${ENDCOLOR}"
